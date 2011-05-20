40111b7c183e743244d9081e93c02d3be5120916
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-05-20 14:06:45 +0200

    Added initial rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TofuWeb::Application.config.secret_token = 'a156bb088cd36c0b84e12079a73c586317fc02388315bf1d3ad3d332b53a5383289e28a1185f1df9c5088b6af9218facd534b6974ca33072962d8ff0ab4edc34'
