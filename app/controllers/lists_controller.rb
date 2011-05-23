3bf17e534dbfe0ec44efee83458d840826a6fede
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-05-23 14:44:52 +0200

    Made lists controller output UTF-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,13 +19,13 @@ class ListsController < ApplicationController
   def todo_lists
     @todo_lists = @dropbox_session.list(LIST_BASE) or return nil
     @todo_lists = @todo_lists.select{|file| !file["is_dir"] && file["path"] =~ /.txt$/}
-    @todo_lists = @todo_lists.map{|file| File.basename(file["path"]).split(/\.txt/).first}
+    @todo_lists = @todo_lists.map{|file| File.basename(file["path"]).split(/\.txt/).first.force_encoding('UTF-8')}
     @todo_lists = @todo_lists.select{|file| file !~ /^archive./ && file !~ /conflicted copy/}
   end
 
   def todo_items(list)
     @dropbox_session.download("#{LIST_BASE}/#{list}.txt").scan(/^\s*(?:\((\w)\)\s*)?(.*)$/).map do |prio, text|
-      item = {:text => text}
+      item = {:text => text.force_encoding('UTF-8')}
       item[:prio] = prio if prio
       tags = text.scan(/@\S+/)
       item[:tags] = tags unless tags.empty?
