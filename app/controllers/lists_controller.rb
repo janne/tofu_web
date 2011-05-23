de363f8fa24416ecf7fcba490706a29e0044ab04
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-05-23 11:19:24 +0200

    Added todo_items method that outputs array of hashes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,4 +21,14 @@ class ListsController < ApplicationController
     @todo_lists = @todo_lists.map{|file| File.basename(file["path"]).split(/\.txt/).first}
     @todo_lists = @todo_lists.select{|file| file !~ /^archive./ && file !~ /conflicted copy/}
   end
+
+  def todo_items(list)
+    @dropbox_session.download("#{LIST_BASE}/#{list}.txt").scan(/^\s*(?:\((\w)\)\s*)?(.*)$/).map do |prio, text|
+      item = {:text => text}
+      item[:prio] = prio if prio
+      tags = text.scan(/@\S+/)
+      item[:tags] = tags unless tags.empty?
+      item
+    end
+  end
 end
