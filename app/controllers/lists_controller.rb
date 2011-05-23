cbad18993b1228e92f327f186f5640a1652244da
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-05-23 11:19:49 +0200

    Removed caching from todo_lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,6 @@ class ListsController < ApplicationController
   private
 
   def todo_lists
-    return @todo_lists if defined?(@todo_lists)
     @todo_lists = @dropbox_session.list(LIST_BASE) or return nil
     @todo_lists = @todo_lists.select{|file| !file["is_dir"] && file["path"] =~ /.txt$/}
     @todo_lists = @todo_lists.map{|file| File.basename(file["path"]).split(/\.txt/).first}
